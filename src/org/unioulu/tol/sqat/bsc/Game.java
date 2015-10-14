dd58cc5895531abf1dee58b166e38d0732eeb8cd
Itir Karac <karacitir@gmail.com>
2015-10-14 13:26:02 +0300

    methods added

--- src/org/unioulu/tol/sqat/bsc/Game.java
+++ src/org/unioulu/tol/sqat/bsc/Game.java
@@ -1,5 +1,27 @@
 package org.unioulu.tol.sqat.bsc;
 
-public class Game {
-
+public class BowlingGame {
+	//a bowling game is made of (at least) 10 frames
+	private List<Frame> frames = new ArrayList<Frame>();
+	private Frame bonus;
+	
+	public BowlingGame(){}
+	
+	public void addFrame(Frame frame){
+		//to be implemented
+	}
+	
+	public void setBonus(int firstThrow, int secondThrow) {
+		//to be implemented
+	}
+	
+	public int score(){
+		//to be implemented
+		return 0;
+	}
+	
+	public boolean isNextFrameBonus(){
+		//to be implemented
+		return false;
+	}
 }
