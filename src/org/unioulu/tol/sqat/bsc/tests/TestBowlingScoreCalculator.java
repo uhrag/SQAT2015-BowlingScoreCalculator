696a260203fbbbb5befd3eaab6dae5501a05b462
somename <someemail>
2015-10-14 16:15:07 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -2,28 +2,31 @@ package org.unioulu.tol.sqat.bsc.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.BowlingGame;
+import org.unioulu.tol.sqat.bsc.Frame;
+
 
 public class TestBowlingScoreCalculator {
 
-	@Test
-	public void testFrameScore() {
-		//
-		int score = 0;
-		//act
-		
-		//assert
-		assertEquals(0,score);
+	Frame tFrame;
+	BowlingGame tBowlingGame;
+	
+	@Before
+	public void setup(){
+		tFrame = new Frame(0, 0);
 	}
 	
 	@Test
-	public void testTotalFrameScore() {
+	public void testSingleFrameScore() {
 		//
-		int score = 0;
+		
 		//act
+		tFrame.addFrame(4,6);
 		
 		//assert
-		assertEquals(5,score);
+		assertEquals(10,tFrame(score));
 	}
 	
 	
