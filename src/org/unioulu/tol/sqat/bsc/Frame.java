504b07edb900d8395395f08ec076a78031123075
Itir Karac <karacitir@gmail.com>
2015-10-14 13:24:39 +0300

    methods added

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -1,5 +1,41 @@
 package org.unioulu.tol.sqat.bsc;
 
 public class Frame {
+	private int firstThrow;
+	private int secondThrow;
+	
+	public Frame(int firstThrow, int secondThrow){
+		this.firstThrow = firstThrow;
+		this.secondThrow = secondThrow;
+	}
+	
+	//the score of a single frame
+	public int score(){
+		//to be implemented
+		return 0;
+	}
 
+	//returns whether the frame is a strike or not
+	public boolean isStrike(){
+		//to be implemented
+		return false;
+	}
+	
+	//return whether a frame is a spare or not
+	public boolean isSpare(){
+		//to be implemented
+		return false;
+	}
+
+	//return whether this is the last frame of the match
+	public boolean isLastFrame(){
+		//to be implemented
+		return false;
+	}
+
+	//bonus throws
+	public int bonus(){
+		//to be implemented
+		return 0;
+	}
 }
